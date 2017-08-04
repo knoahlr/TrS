4664835876d22515d97d32389a873312824a0837
Knoahlr <noahlangat@cmail.carleton.ca>
2017-08-04 00:48:29 -0400

    Minor progress to Django website

--- Tr/TrDisplay/urls.py
+++ Tr/TrDisplay/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
 
     url(r'^$', views.tableView.as_view(), name='index'),
+    url(r'^$(?P<TRNO>[0-9]+)', views.TrUpdate.as_view(), name='data'),
 
 ]
--- Tr/TrDisplay/views.py
+++ Tr/TrDisplay/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import UpdateView
 from .models import TrClass
 
-def index(request):
-    Trs = TrClass.objects.all()
-    return render(request,'index.html', context={'Trs':Trs})
+# def index(request):
+#     Trs = TrClass.objects.all()
+#     return render(request,'index.html', context={'Trs':Trs})
 
-
+# def data
 class tableView(ListView):
     model = TrClass
     template_name = 'index.html'
+
+class TrUpdate(UpdateView):
+    model = TrClass
+    template_name = 'data.html'
+    fields = ['Prior_Int', 'owner', 'comments' ]
+
