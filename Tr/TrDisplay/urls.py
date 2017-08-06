1c75954dbc6bc160f54870bb61542314355a10e3
Knoahlr <noahlangat@cmail.carleton.ca>
2017-08-06 12:27:49 -0400

    Editing Database from website is now functional

--- Tr/TrDisplay/urls.py
+++ Tr/TrDisplay/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.core.urlresolvers import reverse_lazy
 
 
 urlpatterns = [
 
     url(r'^$', views.tableView.as_view(), name='index'),
-    url(r'^UpdateTr/(?P<pk>[0-9]+)', views.TrUpdate.as_view(), name='data'),
+    url(r'^UpdateTr/(?P<pk>[0-9]+)', views.TrUpdate.as_view(success_url=reverse_lazy('index')), name='data')
 
 ]
--- Tr/TrDisplay/views.py
+++ Tr/TrDisplay/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import TrClass
+from .models import TrClass, TrForms
 
 from django.views.generic.edit import UpdateView
-
 
 def index(request):
     Trs = TrClass.objects.all()
@@ -16,11 +15,11 @@ class tableView(ListView):
     template_name = 'index.html'
 
 class TrUpdate(UpdateView):
-
+    
+    form_class = TrForms
     model = TrClass
     TrNO = None
     template_name = 'data.html'
-    fields = ['Prior_Int', 'owner', 'comments' ]
     context_object_name = 'TrObject'
     
     def get_object(self):
